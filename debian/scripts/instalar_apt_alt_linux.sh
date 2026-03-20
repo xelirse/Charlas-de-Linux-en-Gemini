56f51ec9a732b770e906d227dd10d02fd5b76bfc
xelirse <passymas@gmail.com>
2026-03-20 08:36:01 +0000

    Update instalar_apt_alt_linux.sh

--- debian/scripts/instalar_apt_alt_linux.sh
+++ debian/scripts/instalar_apt_alt_linux.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rpm --force -i -ivh --nodeps --ignoreos  rpm-4.13.0.1-alt44.x86_64.rpm getopt-2.39.2-alt2.x86_64.rpm librpm7-4.13.0.1-alt44.x86_64.rpm libcap-2.69-alt1.x86_64.rpm libdb5.3-5.3.28-alt7.x86_64.rpm compat-db47-4.7.25-28.amzn2.x86_64.rpm lua53-libs-5.3.5-7.amzn2.x86_64.rpm libsha1detectcoll1-1.0.3-2.18.x86_64.rpm compat-db-headers-4.7.25-28.amzn2.noarch.rpm
+rpm --force -i -ivh --nodeps --ignoreos apt-0.5.15lorg2-alt100.x86_64.rpm rpm-4.13.0.1-alt44.x86_64.rpm getopt-2.39.2-alt2.x86_64.rpm librpm7-4.13.0.1-alt44.x86_64.rpm libcap-2.69-alt1.x86_64.rpm libdb5.3-5.3.28-alt7.x86_64.rpm compat-db47-4.7.25-28.amzn2.x86_64.rpm lua53-libs-5.3.5-7.amzn2.x86_64.rpm libsha1detectcoll1-1.0.3-2.18.x86_64.rpm compat-db-headers-4.7.25-28.amzn2.noarch.rpm

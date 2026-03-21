492ce854ba4e76d2ef5533ac1a59158e0faa0469
xelirse <passymas@gmail.com>
2026-03-21 13:48:33 +0000

    Update dir_n_instalar_libc_evadiendo_timeshit.sh

--- debian/scripts/dir_n_instalar_libc_evadiendo_timeshit.sh
+++ debian/scripts/dir_n_instalar_libc_evadiendo_timeshit.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Creamos una carpeta temporal para esconder los hooks
-mkdir -p /n/etc/pacman.d/hooks_backup
+mkdir -pv /n/etc/pacman.d/hooks_backup
 
 # Movemos los hooks de la carpeta de pacman a la temporal
 # Esto evitará que se active Timeshift
 mv /n/etc/pacman.d/hooks/* /n/etc/pacman.d/hooks_backup/
 
 # Instalar libc
-pacman-static -Syu --overwrite="*" glibc glibc-locales lib32-glibc
+pacman-static -Syu --overwrite="*" --root /n glibc glibc-locales lib32-glibc

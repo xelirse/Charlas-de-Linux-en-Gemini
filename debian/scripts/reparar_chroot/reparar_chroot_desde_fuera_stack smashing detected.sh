f688dc86f571a0376cc7d5eb5e3aa3150e517b6f
xelirse <passymas@gmail.com>
2026-03-22 17:36:46 +0000

    Update reparar_chroot_desde_fuera_stack smashing detected.sh

--- debian/scripts/reparar_chroot/reparar_chroot_desde_fuera_stack smashing detected.sh
+++ debian/scripts/reparar_chroot/reparar_chroot_desde_fuera_stack smashing detected.sh
@@ -12,19 +12,17 @@ cp -vf etc/resolv.conf /etc/pacman.conf
 # Para dpkg-deb
 pacman -S --overwrite="*" dpkg strace busybox nix-busybox gdb
 
-if [ ! "/usr/lib/x86_64-linux-gnu/libc.so.6" ] ; then
-    mkdir -p tmp/libc_rescue
-    dpkg-deb -x var/cache/apt/archives/libc6_2.42-13_amd64.deb tmp/libc_rescue
-    cp -rf tmp/libc_rescue/* /n
-    /usr/sbin/ldconfig
-    dpkg --configure -a
-    busybox ls -lh lib/libc.so*
-    busybox rm -v usr/lib/libc.so.6.broken
-    busybox rm -v /usr/lib/libc.so.6.broken
-    busybox rm -v usr/lib/x86_64-linux-gnu/libc.so
-    busybox rm -v usr/lib/x86_64-linux-gnu/libc.so.6
-    busybox rm -v usr/lib/x86_64-linux-gnu/libc.so.6.0
-    busybox rm -v usr/lib/x86_64-linux-gnu/libc.so.6.0.0
-    busybox ls -lh usr/lib/x86_64-linux-gnu/libc.so.6*
-    busybox cp -vf lib/libc.* usr/lib/x86_64-linux-gnu
-fi
+mkdir -p tmp/libc_rescue
+dpkg-deb -x var/cache/apt/archives/libc6_2.42-13_amd64.deb tmp/libc_rescue
+cp -rf tmp/libc_rescue/* /n
+/usr/sbin/ldconfig
+dpkg --configure -a
+busybox ls -lh lib/libc.so*
+busybox rm -v usr/lib/libc.so.6.broken
+busybox rm -v /usr/lib/libc.so.6.broken
+busybox rm -v usr/lib/x86_64-linux-gnu/libc.so
+busybox rm -v usr/lib/x86_64-linux-gnu/libc.so.6
+busybox rm -v usr/lib/x86_64-linux-gnu/libc.so.6.0
+busybox rm -v usr/lib/x86_64-linux-gnu/libc.so.6.0.0
+busybox ls -lh usr/lib/x86_64-linux-gnu/libc.so.6*
+busybox cp -vf lib/libc.* usr/lib/x86_64-linux-gnu

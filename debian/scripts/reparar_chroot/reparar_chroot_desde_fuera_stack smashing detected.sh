a10324278595bac77d0370ff4ce7a3a74d7ccfa0
xelirse <passymas@gmail.com>
2026-03-22 08:31:00 +0000

    Update reparar_chroot_desde_fuera_stack smashing detected.sh

--- debian/scripts/reparar_chroot/reparar_chroot_desde_fuera_stack smashing detected.sh
+++ debian/scripts/reparar_chroot/reparar_chroot_desde_fuera_stack smashing detected.sh
@@ -1,20 +1,30 @@
 #!/bin/sh
 
+# Remover bloqueo de pacman
+rm /var/lib/pacman/db.lck
+
+# El internet
+cp -vf etc/resolv.conf /etc/resolv.conf
+
+# Configuración de pacman
+cp -vf etc/resolv.conf /etc/pacman.conf
+
+# Para dpkg-deb
+pacman -S --overwrite="*" dpkg strace busybox nix-busybox
+
 if [ ! "/usr/lib/x86_64-linux-gnu/libc.so.6" ] ; then
-    mkdir -p /tmp/libc_rescue
-    dpkg-deb -x /var/cache/apt/archives/libc6_2.42-13_amd64.deb /tmp/libc_rescue
-    cp -rf /tmp/libc_rescue/* /
+    mkdir -p tmp/libc_rescue
+    dpkg-deb -x var/cache/apt/archives/libc6_2.42-13_amd64.deb tmp/libc_rescue
+    cp -rf tmp/libc_rescue/* /n
     /usr/sbin/ldconfig
     dpkg --configure -a
-    cp -vf /lib/libc.* /usr/lib/x86_64-linux-gnu
-    cp: target '/usr/lib/x86_64-linux-gnu': No such file or directory
-    busybox cp -vf /lib/libc.* /usr/lib/x86_64-linux-gnu
-    busybox ls -lh /lib/libc.so*
+    busybox ls -lh lib/libc.so*
+    busybox rm -v usr/lib/libc.so.6.broken
     busybox rm -v /usr/lib/libc.so.6.broken
-    busybox rm -v /usr/lib/x86_64-linux-gnu/libc.so
-    busybox rm -v /usr/lib/x86_64-linux-gnu/libc.so.6
-    busybox rm -v /usr/lib/x86_64-linux-gnu/libc.so.6.0
-    busybox rm -v /usr/lib/x86_64-linux-gnu/libc.so.6.0.0
-    busybox cp -vf /lib/libc.* /usr/lib/x86_64-linux-gnu
-    busybox ls -lh /usr/lib/x86_64-linux-gnu/libc.so.6*
+    busybox rm -v usr/lib/x86_64-linux-gnu/libc.so
+    busybox rm -v usr/lib/x86_64-linux-gnu/libc.so.6
+    busybox rm -v usr/lib/x86_64-linux-gnu/libc.so.6.0
+    busybox rm -v usr/lib/x86_64-linux-gnu/libc.so.6.0.0
+    busybox ls -lh usr/lib/x86_64-linux-gnu/libc.so.6*
+    busybox cp -vf lib/libc.* usr/lib/x86_64-linux-gnu
 fi

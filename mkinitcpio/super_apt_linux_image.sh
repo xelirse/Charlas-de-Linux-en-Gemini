c373c163c0a5e98855e5a8e4647db5e1d65d9fdf
xelirse <passymas@gmail.com>
2026-03-22 23:14:31 +0000

    Create super_apt_linux_image.sh

---
+++ mkinitcpio/super_apt_linux_image.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+apt install linux-headers-6.19.8+deb14-amd64 linux-headers-6.19.8+deb14-cloud-amd64 linux-headers-6.19.8+deb14-rt-amd64 linux-image-6.19.8+deb14-amd64 linux-image-6.19.8+deb14-amd64-dbg linux-image-6.19.8+deb14-cloud-amd64 linux-image-6.19.8+deb14-cloud-amd64-dbg linux-image-6.19.8+deb14-rt-amd64 linux-image-6.19.8+deb14-rt-amd64-dbg linux-image-amd64 linux-image-amd64-dbg linux-image-amd64-signed-template linux-image-cloud-amd64 linux-image-cloud-amd64-dbg linux-image-rt-amd64 linux-image-rt-amd64-dbg u-boot-efi-dtb 

2d964e4748b397922c3267daab282c004210267a
xelirse <passymas@gmail.com>
2026-03-29 04:18:57 -0300

    Add comment for Xsetup editing in maxima_resolucion.sh
    Add comment to guide editing Xsetup for display resolution.

--- debian/scripts/maxima_resolucion.sh
+++ debian/scripts/maxima_resolucion.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+# nano /usr/share/sddm/scripts/Xsetup
 xrandr --output VGA-1 --mode $( xrandr | grep -E "^ +[0-9]+x[0-9]+" | sort -nr -t 'x' -k1,1 -k2,2 | head -n1  | awk '{print $1}' )

4d64f8dd1c6dceed134bffec87a827b8f266f834
xelirse <passymas@gmail.com>
2026-03-29 15:57:58 -0300

    Enhance ISO creation script with additional options
    Add options for Rock Ridge extensions and QEMU test command.

--- debian/scripts/crear_iso.sh
+++ debian/scripts/crear_iso.sh
@@ -32,8 +32,13 @@ xorriso -as mkisofs \
   -full-iso9660-filenames \
   -volid "FRANKEINUX" \
   -output frankeinux_dist.iso \
+  -J -R \
   -graft-points \
   -b boot/grub/eltorito.img \
   -no-emul-boot -boot-load-size 4 -boot-info-table \
   --grub2-boot-info \
   /iso
+
+echo "Comando para probar el iso
+qemu-system-x86_64 -enable-kvm -m 2G -cdrom frankeinux_dist.iso
+"

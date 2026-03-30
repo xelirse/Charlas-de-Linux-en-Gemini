6c9ff2b7a81fd7c0b2ca2e4fc45c0c0be5ab13dc
xelirse <passymas@gmail.com>
2026-03-29 21:41:10 -0300

    Update ISO creation script for live environment

--- debian/scripts/crear_iso.sh
+++ debian/scripts/crear_iso.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-mkdir -pv /iso/boot/grub
 mkdir -pv /iso/live
-mkdir -pv /iso/boot/grub/x86_64-emu
+mkdir -pv /iso/live/boot/grub
+mkdir -pv /iso/live/boot/grub/x86_64-emu
 
 cp -v /vmlinuz /iso/live/vmlinuz
 # cp -v /usr/lib/grub/i386-pc/eltorito.img /iso/boot/grub/eltorito.img
-cp -v /boot/grub/x86_64-emu/kernel.img /iso/boot/grub/x86_64-emu/kernel.img
-cp -v /boot/initrd.img-6.19-x86_64 /iso/live/initrd.img-6.19-x86_64.zstd
-cp -rv /boot/grub/i386-pc /iso/boot/grub
-cp -rv /boot/grub/x86_64-emu /iso/boot/grub
+cp -v /boot/grub/x86_64-emu/kernel.img     /iso/live/boot/grub/x86_64-emu/kernel.img
+cp -v /boot/initrd.img-6.19-x86_64         /iso/live/initrd.img-6.19-x86_64.zstd
+cp -rv /boot/grub/i386-pc                  /iso/live/boot/grub
+cp -rv /boot/grub/x86_64-emu               /iso/live/boot/grub
 
 echo "set default=0
 set timeout=3
 menuentry \"Frankeinux Live (Debian Sid)\" {
     linux /live/vmlinuz boot=live quiet
     initrd /live/initrd.img-6.19-x86_64.zstd
-}" > "/iso/boot/grub/grub.cfg"
+}" > "/iso/live/boot/grub/grub.cfg"
 
 mksquashfs / /iso/live/filesystem.squashfs -e \
 proc sys dev run tmp mnt media iso trixie debian_trixie otro \
@@ -57,10 +57,10 @@ xorriso -as mkisofs \
   -output /iso/frankeinux.iso \
   -J -R \
   -graft-points \
-  -b /boot/grub/x86_64-emu/kernel.img \
+  -b /boot/grub/i386-pc/boot.img \
   -no-emul-boot -boot-load-size 4 -boot-info-table \
   --grub2-boot-info \
-  /iso
+  /iso/live
 
 echo "Comando para probar el iso
 qemu-system-x86_64 -enable-kvm -m 2G -cdrom /iso/frankeinux.iso

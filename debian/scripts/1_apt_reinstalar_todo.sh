c36364f8fbb0bbeb75f0ed237484abc8a72bdea8
xelirse <passymas@gmail.com>
2026-04-02 17:40:27 -0300

    Update 1_apt_reinstalar_todo.sh

--- debian/scripts/1_apt_reinstalar_todo.sh
+++ debian/scripts/1_apt_reinstalar_todo.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 # apt reinstalar todo menos libc
+#!/bin/sh
+
 $(echo apt reinstall $( apt list --installed 2>/dev/null | \
 	cat - | \
 	sed -E "s/\/(.+)|(L.+)//g" | \
-	grep -Ev "^base_files$" | \
+	grep -Ev "^base-files$" | \
 	grep -Ev "^libc6$" | \
 	tr "\n" " "
 ))

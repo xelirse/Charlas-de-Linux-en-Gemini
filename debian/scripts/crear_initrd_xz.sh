bab95f390fc0f05cdcbeba7000ed3aaee4a49f54
xelirse <passymas@gmail.com>
2026-04-03 20:38:08 -0300

    Update crear_initrd_xz.sh

--- debian/scripts/crear_initrd_xz.sh
+++ debian/scripts/crear_initrd_xz.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+mkdir -p ./lib/modules/6.19.10+deb14-amd64/kernel
+
+cp -ar /lib/modules/6.19.10+deb14-amd64/kernel/drivers/ata ./lib/modules/6.19.10+deb14-amd64/kernel/
+cp -ar /lib/modules/6.19.10+deb14-amd64/kernel/drivers/scsi ./lib/modules/6.19.10+deb14-amd64/kernel/
+cp -ar /lib/modules/6.19.10+deb14-amd64/kernel/drivers/cdrom ./lib/modules/6.19.10+deb14-amd64/kernel/
+cp -ar /lib/modules/6.19.10+deb14-amd64/kernel/fs/isofs ./lib/modules/6.19.10+deb14-amd64/kernel/
+
 depmod -b . 6.19.10+deb14-amd64
-umount -l ./proc ./sys
-find . | cpio -R 0:0 -o -H newc | xz --check=crc32 --threads=0 > ../initrd.img-6.19-x86_64.xz
+
+find . | cpio -R 0:0 -o -H newc | xz > ../initrd.img-6.19-x86_64.xz
+
+cp -vf ../initrd.img-6.19-x86_64.xz ../live/initrd.img-6.19-x86_64.xz

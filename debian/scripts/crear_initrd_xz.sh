0f96a630974b1e8ea5b2c21a616cf61b01d6bd50
xelirse <passymas@gmail.com>
2026-04-03 22:08:05 -0300

    Update crear_initrd_xz.sh

--- debian/scripts/crear_initrd_xz.sh
+++ debian/scripts/crear_initrd_xz.sh
@@ -9,7 +9,7 @@ cp -ar /lib/modules/6.19.10+deb14-amd64/kernel/fs/isofs ./lib/modules/6.19.10+de
 
 depmod -b . 6.19.10+deb14-amd64
 
-find . | cpio -R 0:0 -o -H newc | xz > ../initrd.img-6.19-x86_64.xz
+find . | cpio -R 0:0 -o -H newc | xz --check=crc32 > ../initrd.img-6.19-x86_64.xz
 # find . | cpio -R 0:0 -o -H newc | gzip -9 > ../initrd.img-6.19-x86_64.gz
 
 cp -vf ../initrd.img-6.19-x86_64.xz ../live/initrd.img-6.19-x86_64.xz

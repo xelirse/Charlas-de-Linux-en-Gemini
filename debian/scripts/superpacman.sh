50917317015740e59274bbeb8c67a81068001158
xelirse <passymas@gmail.com>
2026-04-08 09:22:01 -0300

    Update superpacman.sh

--- debian/scripts/superpacman.sh
+++ debian/scripts/superpacman.sh
@@ -48,3 +48,4 @@ pacman -Syu --overwrite="*" jupyter-notebook python-cvxopt python-joblib socat p
 pacman -Syu --overwrite="*" bash-language-server jedi-language-server pyright python-lsp-server texlab typescript-language-server vscode-css-languageserver vscode-html-languageserver vscode-json-languageserver yaml-language-server
 pacman -Syu --overwrite="*" man-db shellcheck shfmt python-msgspec python-mccabe python-pycodestyle python-pydocstyle python-pyflakes python-pylint python-rope autopep8 flake8 python-whatthepatch
 pacman -Syu --overwrite="*" python-tomli-w python-pystemmer python-objgraph python-sphinx
+pacman -Syu --overwrite="*" python-myst-parser texlive-fontsextra texlive-latexextra
